05a18f7062d6f430639908d373010249a6d96f14
Randy Marmer <rmarmer1@yahoo.com>
2016-05-09 23:56:05 -0400

    Session 1; Challenge 6

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -6,5 +6,30 @@
 #
 
 def odds_and_evens(string, return_odds)
+	arr = string.chars
 
+	if return_odds 
+		arr.select.with_index { |el, i| i.odd? }.join.to_s
+		#puts "true"
+	else
+		arr.select.with_index { |el, i| i.even? }.join.to_s
+		#puts "false"
+	end
 end
+
+# These are a couple of other approches from stackoverflow that could be adapted to do this:
+# ... this one I understand ...
+# odd, even = a.partition.each_with_index{ |el, i| i.even? }
+
+# ... but even though this one looks interesting, I don't quite understand how the splat
+# is being used here ...
+# a = ('a'..'z').to_a
+# 
+# a.values_at(* a.each_index.select {|i| i.even?})
+# # => ["a", "c", "e", "g", "i", "k", "m", "o", "q", "s", "u", "w", "y"]
+# 
+# a.values_at(* a.each_index.select {|i| i.odd?})
+# => ["b", "d", "f", "h", "j", "l", "n", "p", "r", "t", "v", "x", "z"]
+
+# odds_and_evens("abcdefg", false)
+
