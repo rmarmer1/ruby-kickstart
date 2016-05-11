d656e6e97ca17ca672715741a2fd0f6b12e75af0
Randy Marmer <rmarmer1@yahoo.com>
2016-05-10 22:08:38 -0400

    Session 2; Challenge 8

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,6 +6,6 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
-def method_name
-	
+def got_three?(elems)
+  elems.each_cons(3).any? { |a,b,c| a == b && b == c}
 end
